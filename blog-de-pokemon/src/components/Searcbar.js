1b4a77e90db4eb01f8bbf6e7f6aa98aa8be13d68
Marycarmen Zambrano <zambranomarycarmen48@gmail.com>
2022-10-02 23:49:32 +0000

    se agrega

--- blog-de-pokemon/src/components/Searcbar.js
+++ blog-de-pokemon/src/components/Searcbar.js
@@ -1,9 +1,24 @@
 import React from "react";
 
-const Searcbar = () => {
-    return(
-        <div>Searcbar</div>
-    )
+const Searchbar = () => {
+    let search = "";
+
+    const onChange = (evt) =>{
+        console.log(evt.target.value);
+        search = evt.target.value;
+    }
+
+    return (
+    <div>
+        <div>
+            <input placeholder="Buscar Pokemon"
+            onChange= {onchange}></input>
+            
+        </div>
+
+        <div>{search}</div>
+    </div>
+    );
 };
 
-export default Searcbar;
+export default Searchbar;
