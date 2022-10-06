9efa79f7b72c68b31febfb183dc12f09bc5cf949
Marycarmen Zambrano <zambranomarycarmen48@gmail.com>
2022-10-06 00:17:44 +0000

    se modifica buscador

--- blog-de-pokemon/src/App.js
+++ blog-de-pokemon/src/App.js
@@ -2,14 +2,24 @@ import React from "react";
 import './App.css';
 import Navbar from "./components/Navbar";
 import Searchbar from "./components/Searchbar";
+import Pokedex from "./components/Pokedex";
+
+const {useState, useEffect} = React;
 
 
-function App() {
+export default function App() {
+  const [pokemons, setPokemons] = useState ([]);
+
+  useEffect(() => {
+    console.log("Inside useEffect");
+  },[])
+
   return (
     <div>
       <Navbar/>
     <div className="App">
       <Searchbar/>
+      <Pokedex/>
 
 
 
@@ -19,4 +29,3 @@ function App() {
   );
 }
 
-export default App;
