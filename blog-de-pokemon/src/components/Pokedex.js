9665e9a98d5d94a9348a33bab8ff5248399752b1
Marycarmen Zambrano <zambranomarycarmen48@gmail.com>
2022-10-09 22:19:32 +0000

    se agrega card

--- blog-de-pokemon/src/components/Pokedex.js
+++ blog-de-pokemon/src/components/Pokedex.js
@@ -2,7 +2,7 @@ import React from "react";
 import Pokemon from "./Pokemon";
 
 const Pokedex = (props) => {
-    const {pokemons} = (props);
+    const {pokemons} = props;
     return(
         <div>
             <div className="header">
--- blog-de-pokemon/src/components/Pokemon.js
+++ blog-de-pokemon/src/components/Pokemon.js
@@ -5,27 +5,27 @@ const Pokemon = (props) => {
     console.log(pokemon)
     return (
         <div className="pokemon-card">
-            <div className="pokemon-img">
-                <img src={pokemon.sprites.front_default}
-                alt={pokemon.name}
-                />
+            <div className="pokemon-img-container">
+                <img 
+                    src={pokemon.sprites.front_default}
+                    alt={pokemon.name}
+                    className="pokemon-img"
+                 />
             </div>
 
-            <div>
-                <div>
+            <div className="card-body">
+                <div className="card-top">
                     <h3>{pokemon.name}</h3>
                     <div>#{pokemon.id}</div>
-                <div>
-              </div>    
+                    </div>
+                    <div className="card-bottom" >
                     <div>
-                        {pokemon.types.pam((type, idx) => {
-                            return(
-                             <div key={idx}>{type.name}</div>
-                            )
+                        {pokemon.types.map((type, idx) => {
+                            return  <div key={idx}>{type.type.name}</div>;
                         })}
                     </div>
                     <div>❤️</div>
-              </div>
+                </div>
             </div>
         </div>
     );
